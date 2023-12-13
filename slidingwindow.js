a8a7d307c87dc20e0890424711444c625c50d536
kizwolak <ki.zwolak@gmail.com>
2023-12-13 03:07:01 +0100

    Initial commit

---
+++ slidingwindow.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let collection = [];
+    let temp = [];
+    let count = 0;
+    let iteration = 0;
+    for (let i = iteration; i < nums.length; i++) {
+        if (count === k) {
+            let max = 0;
+            for (number of temp) {
+                if (number > max) {
+                    max = number;
+                }
+            }
+            iteration += 1;
+            collection.push(max);
+            temp = [];
+            count = 0;
+            i = iteration
+            i--;
+        } else {
+            temp.push(nums[i])
+            count++;
+        }
+
+    }
+    console.log(collection);
+};
+
+maxSlidingWindow([5,3,6,7], 3)
